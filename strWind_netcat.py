8035d28f8cfdd323d3e2758af80c0ecef77432be
J. Delos <julia.delos@ict.nl>
2016-11-01 08:16:20 +0000

    Changed name of the files

--- strWind_netcat.py
+++ strWind_netcat.py
@@ -19,10 +19,11 @@ words = lines.flatMap(lambda line: line.split(" "))
 
 #Count each word in each batch
 pairs = words.map(lambda word: (word, 1))
-wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+wordCountsWind = pairs.reduceByKeyAndWindow(lambda x, y: x + y,lambda x, y: x -y,30,10)
+
 
 # Print the first ten elements of each RDD generated in this DStream to the console
-wordCounts.pprint()
+wordCountsWind.pprint()
 
 ssc.start()             # Start the computation
 ssc.awaitTermination()  # Wait for the computation to terminate
--- streaming_netcat.py
+++ streaming_netcat.py
@@ -19,8 +19,7 @@ words = lines.flatMap(lambda line: line.split(" "))
 
 #Count each word in each batch
 pairs = words.map(lambda word: (word, 1))
-wordCountsWind = pairs.reduceByKeyAndWindow(lambda x, y: x + y,lambda x, y: x -y,30,10)
-
+wordCounts = pairs.reduceByKey(lambda x, y: x + y)
 
 # Print the first ten elements of each RDD generated in this DStream to the console
 wordCounts.pprint()
