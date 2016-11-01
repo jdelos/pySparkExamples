31ba15b1521bbf7a792e51d5d10fe4c1aae7fd51
J. Delos <julia.delos@ict.nl>
2016-11-01 08:12:37 +0000

    Added two streaming examples :W  Changes to be committed: 	new file:   streaming_netcat.py 	new file:   streaming_netcat_wind.py

---
+++ streaming_netcat.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+
+# Create a local StreamingContext with two working thread and batch interval of 1 second
+sc = SparkContext("local[2]", "NetworkWordCount")
+ssc = StreamingContext(sc, 1)
+
+#Reduce logging to only errors
+sc.setLogLevel('WARN')
+
+# Create a DStream that will connect to hostname:port, like localhost:9999
+lines = ssc.socketTextStream("localhost", 9999)
+
+# Split each line into words
+words = lines.flatMap(lambda line: line.split(" "))
+
+#Count each word in each batch
+pairs = words.map(lambda word: (word, 1))
+wordCountsWind = pairs.reduceByKeyAndWindow(lambda x, y: x + y,lambda x, y: x -y,30,10)
+
+
+# Print the first ten elements of each RDD generated in this DStream to the console
+wordCounts.pprint()
+
+ssc.start()             # Start the computation
+ssc.awaitTermination()  # Wait for the computation to terminate
